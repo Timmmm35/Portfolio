6535b5479c1c99a84b87e6e2d31e181fd7ea98ab
Timmmm35 <dequansimmons6@gmail.com>
2025-03-25 21:30:24 -0500

    Deploy Vue + Three.js portfolio

---
+++ my-portfolio/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Welcome to My Portfolio</h1>
+    <ThreeScene />
+  </div>
+</template>
+
+<script>
+import ThreeScene from "./components/ThreeScene.vue";
+
+export default {
+  components: { ThreeScene },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
